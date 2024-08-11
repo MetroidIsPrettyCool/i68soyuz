abeb8c960adeb0729bfc235a9f77ec810687ad0d
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2024-08-11 04:00:44 -0700

    add header file dependencies to makefile

--- makefiles/68k.mk
+++ makefiles/68k.mk
@@ -9,5 +9,10 @@ OBJFILES = i68soyuz.o i68s_handshake.o i68s_read_kbd.o i68s_68k_interrupts.o
 $(OUTFILE): $(OBJFILES)
 	$(CC) $(CFLAGS) -o $@ $^
 
+i68soyuz.o: i68s_sys_break.h i68s_sys_idle.h i68s_sys_input.h i68s_sys_interrupts.h i68s_sys_link.h i68s_sys_matrix.h i68s_sys_output.h i68s_handshake.h i68s_read_kbd.h i68s_version.h i68soyuz.h
+i68s_handshake.o: i68s_sys_link.h i68s_sys_machine.h i68s_version.h i68s_handshake.h
+i68s_read_kbd.o: i68s_sys_break.h i68s_sys_interrupts.h i68s_sys_matrix.h i68s_read_kbd.h
+i68s_68l_interrupts.o: i68s_sys_break.h i68s_sys_interrupts.h i68s_sys_matrix.h
+
 clean:
 	rm -f $(OUTFILE) $(OBJFILES)
