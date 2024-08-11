8e367b7e5e4c14bd81ca0a39dc0e3a8e6138f334
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2024-08-11 13:50:14 -0700

    /actually/ fix "ON" key
    it pays to, y'know, actually READ the documentation

--- src/z80/i68s_z80_break.s
+++ src/z80/i68s_z80_break.s
@@ -3,6 +3,10 @@
         ;; routine assumes that BREAK_COL is 0
 _i68s_sys_break_key:
         GLOBAL _i68s_sys_break_key
-        ld l, (IY + onFlags)    ; insofar as I can tell, onInterrupt is the only field of onFLags
-        ld (IY + onFlags), 0
+        bit onInterrupt, (IY + onFlags)
+        ld l, 0
+        jr z, skip
+        ld l, 1
+skip:
+        res onInterrupt, (IY + onFlags)
         ret
